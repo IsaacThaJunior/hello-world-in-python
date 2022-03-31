4641f58c33398153e17ac140573e35f502ef4ce4
IsaacThaJunior <isaacjunez669@gmail.com>
2022-03-31 07:23:21 +0100

    Lists, and strings

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,24 @@
-print('Hello world')
+# print('Hello world')
+
+# name = 'jose'
+# Age = 3
+
+# print(f'Hello {name}, i am {Age} years old')
+
+# Lists in Python
+# my_list = ['one', 'two', 'three', 'four', 'five']
+
+
+# print(my_list[1:3])
+
+# List can be mutated unlike strings
+# my_list.append('six')
+
+# my_list.pop()
+# print(my_list)
+
+# new_list = ['a', 'e', 'x', 'b', 'c', 'd']
+# num_list = [4, 1, 8, 3]
+
+# new_list.sort()
+# print(new_list)
